9b1048ac9bd789e36b6b0306087c2854ce554aa8
bapuraoghadge29 <bapuraoghadge29@outlook.com>
2026-08-10 12:48:50 +0530

    Restore DemoRedisCache project

---
+++ DemoRedisCache/DemoRedis/Models/AccountSummary.cs
@@ -0,0 +1,15 @@
+namespace DemoRedis.Models;
+public class AccountSummary
+{
+    public int AccountId { get; set; }
+
+    public string CustomerName { get; set; } = null!;
+
+    public decimal Balance { get; set; }
+
+    public decimal AvailableBalance { get; set; }
+
+    public int RewardPoints { get; set; }
+
+    public string AccountStatus { get; set; }= null!;
+}
